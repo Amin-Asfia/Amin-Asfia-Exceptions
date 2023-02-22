b747beb9b25c6fced43685f346350dfa6b6872c2
Amin-Asfia <amin.asfiya@gmail.com>
2023-02-22 07:24:50 +0000

    Exceptions

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -21,7 +21,6 @@ public class Lab {
      * Check out this guide on throwing exceptions: https://rollbar.com/guides/java/how-to-throw-exceptions-in-java/#
      */
     public void mustThrow() throws Exception {
-        //throw new Exception("This is an exception!");
-        int i = 1/0;
+        throw new Exception("This is an exception!");
     }
 }
